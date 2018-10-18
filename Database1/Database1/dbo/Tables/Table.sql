5139713661c3146d80e050ea2770acc75ab9acd0
騏瑋 劉 <xxxx6712@gmail.com>
2018-10-18 13:57:38 +0800

    Edit database table

--- Database1/Database1/dbo/Tables/Table.sql
+++ Database1/Database1/dbo/Tables/Table.sql
@@ -1,6 +1,10 @@
 ﻿CREATE TABLE [dbo].[Table] (
-    [Id]      INT           NOT NULL,
-    [Subject] NVARCHAR (50) NOT NULL,
+    [Id]           INT            NOT NULL,
+    [Subject]      NVARCHAR (50)  NOT NULL,
+    [Content]      NVARCHAR (MAX) NOT NULL,
+    [PostDateTime] DATETIME       NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
