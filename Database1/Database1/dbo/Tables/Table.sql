b908f937bcd4cc6fc1a6b611a949a7261be93b0c
騏瑋 劉 <xxxx6712@gmail.com>
2018-10-19 14:06:52 +0800

    Database1 project update

--- Database1/Database1/dbo/Tables/Table.sql
+++ Database1/Database1/dbo/Tables/Table.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Table] (
     [Id]           INT            NOT NULL,
-    [Subject]      NVARCHAR (50)  NOT NULL,
+    [Subject]      NVARCHAR (80)  NOT NULL,
     [Content]      NVARCHAR (MAX) NOT NULL,
     [PostDateTime] DATETIME       NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
