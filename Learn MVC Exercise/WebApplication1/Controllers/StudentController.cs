f258d5e67c6b112333613781a0c43d5c1d52b965
騏瑋 劉 <xxxx6712@gmail.com>
2019-01-12 15:42:16 +0800

    Create index.cshtml list and new seven Student instence

--- Learn MVC Exercise/WebApplication1/Controllers/StudentController.cs
+++ Learn MVC Exercise/WebApplication1/Controllers/StudentController.cs
@@ -3,16 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class StudentController : Controller
     {
         // GET: Student
-        [ActionName("Default")]
-        public string Index()
+        //[ActionName("Default")]
+        public ActionResult Index()
         {
-            return "Index";
+            var studentList = new List<Student>{
+                            new Student() { StudentId = 1, StudentName = "John", Age = 18 } ,
+                            new Student() { StudentId = 2, StudentName = "Steve",  Age = 21 } ,
+                            new Student() { StudentId = 3, StudentName = "Bill",  Age = 25 } ,
+                            new Student() { StudentId = 4, StudentName = "Ram" , Age = 20 } ,
+                            new Student() { StudentId = 5, StudentName = "Ron" , Age = 31 } ,
+                            new Student() { StudentId = 4, StudentName = "Chris" , Age = 17 } ,
+                            new Student() { StudentId = 4, StudentName = "Rob" , Age = 19 }
+                        };
+            return View(studentList);
         }
     }
 }
