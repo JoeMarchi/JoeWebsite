fdb9ea225fc8814ecdc8a15b5c066e8a4d2c5a50
騏瑋 劉 <xxxx6712@gmail.com>
2019-10-06 19:13:08 +0800

    (Model 1-3) Using orderby Of 'linq to entity'(method-based)

--- Learn MVC Exercise/WebApplication1/Controllers/ArticleController.cs
+++ Learn MVC Exercise/WebApplication1/Controllers/ArticleController.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class ArticleController : Controller
     {
+        
         // GET: Article
         public ActionResult List_Table_OrderByDescending_MSSQL()
         {
+            Article art = new Article();
             
-            return View();
+            return View(art.List_Table_OrderByDescending_MSSQL());
         }
     }
 }
---
+++ Learn MVC Exercise/WebApplication1/Models/Article.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    
+    public class Article
+    {
+        Forum_DBEntities db = new Forum_DBEntities();
+        public List<Article_Tb> List_Table_OrderByDescending_MSSQL()
+        {
+            return db.Article_Tb.OrderByDescending(p => p.NO_F).ToList();
+            
+        }
+    }
+}
